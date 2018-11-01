ac0c814568b82cb103aef60259fd9d03eaeb879c
Мангир Александр <sasha982025@gmail.com>
2018-11-01 15:24:49 +0200

    Updates

---
+++ precache-manifest.37e8ed23e8f7b21c1c4334688a29b883.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/botquery/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "8bef19d8380589f46ac1",
+    "url": "/botquery/static/css/main.31bdbe92.chunk.css"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/botquery/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "84e5b4a785d6a6938778",
+    "url": "/botquery/static/js/1.84e5b4a7.chunk.js"
+  },
+  {
+    "revision": "8cf91591cbc5dc997809",
+    "url": "/botquery/static/js/runtime~main.8cf91591.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/botquery/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/botquery/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/botquery/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/botquery/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/botquery/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/botquery/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/botquery/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/botquery/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/botquery/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/botquery/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/botquery/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "8bef19d8380589f46ac1",
+    "url": "/botquery/static/js/main.8bef19d8.chunk.js"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/botquery/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/botquery/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/botquery/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/botquery/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/botquery/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/botquery/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/botquery/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/botquery/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/botquery/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/botquery/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/botquery/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/botquery/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/botquery/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/botquery/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "84e5b4a785d6a6938778",
+    "url": "/botquery/static/css/1.8df3bca9.chunk.css"
+  },
+  {
+    "revision": "60e61385152b89f3689fd751a8cbe327",
+    "url": "/botquery/index.html"
+  }
+];
