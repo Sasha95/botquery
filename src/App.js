660d1a2b3e758a1b293c9d5ef04c6afdc793db57
Мангир Александр <sasha982025@gmail.com>
2018-11-01 15:48:22 +0200

    change home page path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { HomeWork } from "./components/HomeWork";
 import { StbleInter } from "./components/StbleInter.js";
 import { RussianLevel } from "./components/RussianLevel";
@@ -17,16 +17,18 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <Route component={HomeWork} />
-        <Route exact path="/botquery/" component={HomeWork} />
-        <Route path="/botquery/StableInternet" component={StbleInter} />
-        <Route path="/botquery/RussianLevel" component={RussianLevel} />
-        <Route path="/botquery/Period" component={Period} />
-        <Route path="/botquery/TimeWork" component={TimeWork} />
-        <Route path="/botquery/Experience" component={Experience} />
-        <Route path="/botquery/OurOffer" component={OurOffer} />
-        <Route path="/botquery/Congartiluation" component={Congartiluation} />
-        <Route path="/botquery/Failure" component={Failure} />
+        <Switch>
+          <Route exact path="/botquery/" component={HomeWork} />
+          <Route path="/botquery/StableInternet" component={StbleInter} />
+          <Route path="/botquery/RussianLevel" component={RussianLevel} />
+          <Route path="/botquery/Period" component={Period} />
+          <Route path="/botquery/TimeWork" component={TimeWork} />
+          <Route path="/botquery/Experience" component={Experience} />
+          <Route path="/botquery/OurOffer" component={OurOffer} />
+          <Route path="/botquery/Congartiluation" component={Congartiluation} />
+          <Route path="/botquery/Failure" component={Failure} />
+          <Route component={HomeWork} />
+        </Switch>
       </Fragment>
     );
   }
