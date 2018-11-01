ba98f019304cf4ce57d0b2adb68ced1eeaf2f032
Мангир Александр <sasha982025@gmail.com>
2018-11-01 15:19:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { Route } from "react-router-dom";
+import { HomeWork } from "./components/HomeWork";
+import { StbleInter } from "./components/StbleInter.js";
+import { RussianLevel } from "./components/RussianLevel";
+import { Period } from "./components/Period";
+import { TimeWork } from "./components/TimeWork";
+import { Experience } from "./components/Experience/Experience";
+import { OurOffer } from "./components/OurOffer";
+import { Congartiluation } from "./components/Congartiluation";
+import { Failure } from "./components/Failure";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Route exact path="/" component={HomeWork} />
+        <Route exact path="/StableInternet" component={StbleInter} />
+        <Route exact path="/RussianLevel" component={RussianLevel} />
+        <Route exact path="/Period" component={Period} />
+        <Route exact path="/TimeWork" component={TimeWork} />
+        <Route exact path="/Experience" component={Experience} />
+        <Route exact path="/OurOffer" component={OurOffer} />
+        <Route exact path="/Congartiluation" component={Congartiluation} />
+        <Route exact path="/Failure" component={Failure} />
+      </Fragment>
     );
   }
 }
